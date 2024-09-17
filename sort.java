3ec310bdd338dbaf8b598732dfbea1b3da67a68d
arpil_khunt <137201938+Arpil-Khunt@users.noreply.github.com>
2024-09-17 15:25:15 +0530

    add merge sort

--- sort.java
+++ sort.java
@@ -6,6 +6,51 @@ public class sort {
         selectionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
+  //MergeSort
+    static void mergeSort(int[] arr, int l, int h){
+        if(l<h){
+            int mid = l+(h-l)/2;
+            mergeSort(arr,l,mid);
+            mergeSort(arr,mid+1,h);
+            merge(arr,l,mid,h);
+        }
+    }
+    //merge in place
+    static void merge(int[] arr, int l , int mid, int h){
+        int n1 = mid-l+1;
+        int n2 = h - mid;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+        for(int i = 0; i<n1; i++){
+            left[i] = arr[l+i];
+        }
+        for(int i =0; i<n2; i++){
+            right[i] = arr[mid+1+i];
+        }
+        int i =0 , j = 0;
+        int k = l;
+        while(i<n1 && j < n2){
+            if(left[i] <= right[j]){
+                arr[k] = left[i];
+                i++;
+            }else{
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        //copy remaining element
+        while(i<n1){
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while(j<n2){
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
     static void bubbleSort(int[] arr){
         for(int i =0; i<arr.length; i++){
             for(int j = 1; j<arr.length-i; j++){
